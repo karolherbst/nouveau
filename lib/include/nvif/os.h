316d65660a75c0de4c45e0f7ba110c4aa9362862
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:15 +1000

    platform: remove subclassing of nvkm_device
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- lib/include/nvif/os.h
+++ lib/include/nvif/os.h
@@ -1212,14 +1212,9 @@ struct nouveau_platform_gpu {
 		struct iommu_domain *domain;
 		unsigned long pgshift;
 	} iommu;
-};
 
-struct nouveau_platform_device {
-	struct nouveau_platform_gpu *gpu;
 	int gpu_speedo;
 };
-
-#define nv_device_to_platform(x) NULL
 
 /******************************************************************************
  * endianness (cont'd)
