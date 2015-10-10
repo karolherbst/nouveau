5eb9161ccd96fab0add90114177578f732dd8016
Karol Herbst <nouveau@karolherbst.de>
2015-10-10 16:18:16 +0200

    nouveau/debugfs: add interface for current load
    output of the file will be something like that:
    
    core, mem,  vid,  pci
    0xfd, 0x15, 0x00, 0xa2
    
    v2: relayout the debugfs file
    
    Signed-off-by: Karol Herbst <nouveau@karolherbst.de>

--- drm/nouveau/nvkm/subdev/pmu/base.c
+++ drm/nouveau/nvkm/subdev/pmu/base.c
@@ -175,6 +175,24 @@ nvkm_pmu_recv(struct work_struct *work)
 		  process, message, data0, data1);
 }
 
+#define get_counter_index(v, i) (((v) >> ((i)*8)) & 0xff)
+
+int
+nvkm_pmu_get_perf_data(struct nvkm_pmu *pmu, struct nvkm_pmu_load_data *data)
+{
+	u32 result[2];
+
+	int ret = nvkm_pmu_send(pmu, result, PROC_PERF, PERF_MSG_LOAD, 0, 0);
+	if (ret < 0)
+		return ret;
+
+	data->core = get_counter_index(result[0], PERF_MSG_LOAD_CORE_IDX);
+	data->video = get_counter_index(result[0], PERF_MSG_LOAD_VID_IDX);
+	data->mem = get_counter_index(result[0], PERF_MSG_LOAD_MEM_IDX);
+	data->pcie = get_counter_index(result[0], PERF_MSG_LOAD_PCIE_IDX);
+	return 0;
+}
+
 static void
 nvkm_pmu_intr(struct nvkm_subdev *subdev)
 {
