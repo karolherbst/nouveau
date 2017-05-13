66c6199b57a07d5b8da49b63729a0532354b700b
Karol Herbst <karolherbst@gmail.com>
2017-05-13 21:50:47 +0200

    pmu: add counters.slots field
    v3: seperated change into own commit
    v4: rename
    
    Signed-off-by: Karol Herbst <karolherbst@gmail.com>

--- drm/nouveau/nvkm/subdev/pmu/gk110.c
+++ drm/nouveau/nvkm/subdev/pmu/gk110.c
@@ -88,6 +88,7 @@ gk110_pmu = {
 	.code.size = sizeof(gk110_pmu_code),
 	.data.data = gk110_pmu_data,
 	.data.size = sizeof(gk110_pmu_data),
+	.counters.slots = 8,
 	.enabled = gf100_pmu_enabled,
 	.reset = gf100_pmu_reset,
 	.init = gt215_pmu_init,
