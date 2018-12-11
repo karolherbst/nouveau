5a1c4ae2db2cd8b048ab9163aea03764c4fae9a5
Ben Skeggs <bskeggs@redhat.com>
2018-12-11 14:50:02 +1000

    core: recognise TU104
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- bin/nv_rffunc.h
+++ bin/nv_rffunc.h
@@ -13,7 +13,7 @@ nv_rfb(struct nvif_device *device, u64 offset)
 	u64 addr = (offset &  (PAGE_SIZE - 1));
 
 	if (device->info.family < NV_DEVICE_INFO_V0_TNT ||
-	    device->info.family > NV_DEVICE_INFO_V0_VOLTA) {
+	    device->info.family > NV_DEVICE_INFO_V0_TURING) {
 		printk("unsupported chipset\n");
 		exit(1);
 	}
